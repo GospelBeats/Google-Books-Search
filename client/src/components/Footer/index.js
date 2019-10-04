8acde4c0e89518702053a20e716cd4e6a18abdb6
Cory <cyb12345@comcast.net>
2019-10-04 16:35:10 -0400

    init commit

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer navbar-fixed-bottom text-center heading-subtitle">
+      ALL Rights Reserved
+    </footer>
+  );
+};
+
+export default Footer;
